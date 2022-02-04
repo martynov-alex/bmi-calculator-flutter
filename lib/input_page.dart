85ba7c548880a250b2ba1796791da6070b67e08e
Alex Martynov <martynov.alex@gmail.com>
2022-02-04 12:07:34 +0600

    Add font_awesome_flutter package

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const double widgetsIndent = 8.0;
 const double bottomContainerHeight = 80.0;
@@ -67,17 +68,19 @@ class _InputPageState extends State<InputPage> {
 // После чего будет автоматически создан новый класс из выбранного виджета
 class ReusableCard extends StatelessWidget {
   // Чтоб сделать параметр обязательным нужно добавить @required
-  ReusableCard({@required this.cardColor});
+  ReusableCard({@required this.cardColor, this.cardChild});
 
   // final в начале означает, что данный параметр неизменный, после создания
   // экземпляра каласса уже нельзя будет поменять это свойство, т.е. класс
   // станет @immutable т.к. он наследуется от StatelessWidget. Есть только один
   // путь изменить StatelessWidget - уничтожить старый и создать новый
   final Color cardColor;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(widgetsIndent),
       padding: EdgeInsets.all(widgetsIndent * 2),
       decoration: BoxDecoration(
